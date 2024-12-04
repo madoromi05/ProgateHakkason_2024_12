cba64d8da18a43a0d234eea1f024ed83346158dc
DESKTOP-5J2G9A1\24RD164的場 <24rd164@ms.dendai.ac.jp>
2024-12-04 16:33:18 +0900

    いったん見れるように

---
+++ frontend/src/timeline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './timeline.css'; // 必要に応じてスタイルを作成
+
+function Timeline() {
+  return (
+    <div className="timeline-container">
+      <h1>タイムライン</h1>
+    </div>
+  );
+}
+
+export default Timeline;
