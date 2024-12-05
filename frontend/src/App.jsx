91ce3e56b795b48bd9e049f5a045381a9f9f18e0
DESKTOP-5J2G9A1\24RD164的場 <24rd164@ms.dendai.ac.jp>
2024-12-06 02:36:21 +0900

    indexからtimelineに移動できるようにした。

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,6 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div className="result_button">
+        <a href="./timeline.html">結果を見る</a>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
