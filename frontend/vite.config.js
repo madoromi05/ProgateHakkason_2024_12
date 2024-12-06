36f5721c589afbf01d2c8b418582f47f6ef2b5eb
madoromi <madoromi.0501.fan@gmail.com>
2024-12-07 05:10:24 +0900

    react-domの依存環境の修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+export default defineConfig({
+  ssr: {
+    noExternal: ['react-icons'],
+  },
+});
+
+/*
 // https://vite.dev/config/
 export default {
   build: {
@@ -9,3 +16,4 @@ export default {
     },
   },
 };
+*/
